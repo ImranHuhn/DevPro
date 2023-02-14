1c56c70feb4a0fd55fdfdaa52bb863d897400b64
ImranRovano <imranhuhn1@gmail.com>
2023-02-14 16:16:20 -0500

    feb14

--- array/script.js
+++ array/script.js
@@ -52,8 +52,9 @@ Array.prototype.find = function(callback){ // callback is the paramater used fro
 
 console.log(data3.find((el) => el === 3)); //this is an arraow function that uses el as a parameter. Curly brackets and return keyword are not necessary if there is only one statement and returns a value. the arguement, is (el) => el === 3)
 
+console.log('==================================================')
 /////////////////////////////////////////////
-// Pick 10 favorite methods, and recreate with prototype method
+// Pick 10 favorite methods, and recreate with prototype method.  
 
 Array.prototype.map
 Array.prototype.reduce
@@ -70,29 +71,86 @@ Array.prototype.splice
 
 
 ///////////////////////////////////
-// map method
+// 1) map method
 const array1 = [1,2,3,4]
 
+console.log('test if map method exist:')
 console.log(Array.prototype.map) //test if method exist
+
+console.log('first test if map method works:')
 console.log(array1.map(x => x)) //test to check array
+
+console.log('second test if map method works:')
 console.log(array1.map(x => x * 2)) //test to multiply array by 2
+
 Array.prototype.map = undefined; //remove method from prototype
+
+console.log('test if map method is removed:')
 console.log(Array.prototype.map) //check if prototype indeed doesn't exist
 
+//creating a constructor for array prototypes
 Array.prototype.map = function(callback){ //created map method
-    let newArray = [];
+    const newArray = [];
     for(let i = 0; i < this.length; i++){
+        // newArray += callback(this[i]); //this is for concatinating characters
         newArray.push(callback(this[i]));
-        // newArray += callback(this[i]);
     }
     return newArray;
 }
-console.log('new array: ' + array1.map(x => x))
-console.log('new array: ' + array1.map(x => x * 2))
 
+console.log('first test for NEW map method')
+console.log(array1.map(x => x)) // first test for new method
+
+console.log('second test for NEW map method')
+console.log(array1.map(x => x * 2)) //second test for new method
+
+console.log('==================================================')
 /////////////////////////////////////
-// reduce method
+// 2) reduce method
 
 const array2 = [1,2,3,4]
 
-console.log(Array.prototype.reduce)
+console.log('test if reduce method exist:')
+console.log(Array.prototype.reduce) //test if method exist
+
+console.log('first test if reduce method works:')
+console.log(array2.reduce(x => x)) //test to check array
+
+console.log('second test if reduce method works:')
+console.log(array2.reduce(x => x * 2)) //test to multiply array by 2
+
+Array.prototype.reduce = undefined; //remove method from prototype
+
+console.log('test if reduce method is removed:')
+console.log(Array.prototype.reduce) //check if prototype indeed doesn't exist
+
+
+Array.prototype.reduce = function(callbackFn, initialValue){
+    let accumulator = 0;
+    for(let i = 0; i < this.length; i++){
+        // accumulator = callbackFn(this[i]);
+        // console.log(typeof(this[i]))
+        // console.log(this[i])
+
+        //if initialValue is specified
+        if(initialValue != undefined){
+            accumulator += initialValue;
+
+        //if initialValue is not specified
+        } else if(initialValue == undefined) {
+            accumulator = this[0];
+        } else {
+            console.log('error')
+        }
+    }
+    return accumulator;
+}
+
+console.log(array2.reduce(x => x,9))
+console.log(array2.reduce(x => x * 2))
+
+// callbackFn
+// A function to execute for each element in the array. Its return value becomes the value of the accumulator parameter on the next invocation of callbackFn. For the last invocation, the return value becomes the return value of reduce().
+
+// accumulator
+// The value resulting from the previous call to callbackFn. On first call, initialValue if specified, otherwise the value of array[0].

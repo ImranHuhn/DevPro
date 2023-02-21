5db872dbb053296e17eb7a393a73b28a982dbe24
ImranRovano <imranhuhn1@gmail.com>
2023-02-20 23:23:04 -0500

    02202023 2323

--- Typewriter/script.js
+++ Typewriter/script.js
@@ -2,5 +2,9 @@ const text = document.getElementById('text')
 
 document.addEventListener('keydown', (e) => {
     text.innerText += e.key;
+    if(e.code == 'Space') {
+        text.innerHTML += '&nbsp';
+    }
     console.log(e.key)
+    console.log(e.code)
 });

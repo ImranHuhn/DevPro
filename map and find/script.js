a16d5af67e22d24f5bdae305295ca372cfec6b12
ImranRovano <imranhuhn1@gmail.com>
2023-02-22 16:06:47 -0500

    02222023 1606

---
+++ map and find/script.js
@@ -0,0 +1,28 @@
+const colors = [
+    { id: 1, color: "red" },
+    { id: 2, color: "blue" },
+    { id: 3, color: "green" }
+  ];
+  const data = [
+    {
+      colorId: 1
+    },
+    {
+      colorId: 2
+    },
+    {
+      colorId: 3
+    }
+  ];
+  
+  export function mapColorsToData(data, colors) {
+    // data.map()
+    return;
+  }
+  
+
+//   const expected = [
+//     { colorId: 1, color: "red" },
+//     { colorId: 2, color: "blue" },
+//     { colorId: 3, color: "green" }
+//   ];

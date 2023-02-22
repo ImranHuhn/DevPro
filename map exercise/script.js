4ba6137a7b0d243f4731815badc1b8ae8f88f91e
ImranRovano <imranhuhn1@gmail.com>
2023-02-22 00:50:14 -0500

    02222023 0050

---
+++ map exercise/script.js
@@ -0,0 +1,29 @@
+const numbers = [1,2,3,4,5,6,7,8];
+const names = ['john smith', 'james bond', 'lara croft', 'tony stark', 'peter parker']
+
+// given the numbers array, use .map to return a new array
+// that has all the items squared
+// expected output: [1,4,9,16...]
+const squareNumbers = numbers.map(function(num){}); // fix this
+
+// given the names array, use .map to return a new array
+// that has the first letter of each word from with the string uppercased
+// expected output: ['John Smith', 'James Bond'...]
+const uppercasedNames = names.map(function(fullName){}); // fix this
+
+// given the numbers array, use .map to return a new array
+// return a new array that hass all the even numbers doubled
+// expected output: [1,4,3,8,5,12 ...]
+const doubleEvens = numbers.map(function(num){}); // fix this
+
+// given the names array, use .map to return a new array
+// that contains objects with a key value that contains 
+// the current string and a length value that contains the length of the current string
+// expected output: [{value: 'john smith', length: 10} ...]
+const namesToObject = names.map(function(fullName){}); // fix this
+
+// given the names array, use map to return an object that contains a letter key 
+// that contains all the letters inside an array
+//expected output: [{value: 'john smith', letters: ['j', 'o', 'h', 'n', 's', 'm', 'i', 't', 'h']}]
+
+const namesToObjectAndLetters = names.map(function(fullName){}); // fix this

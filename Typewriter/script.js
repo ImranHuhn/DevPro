f5d93b2a89253bb4b8cb3088cf909bb954657978
ImranRovano <imranhuhn1@gmail.com>
2023-02-22 15:01:39 -0500

    02222023 1501

--- Typewriter/script.js
+++ Typewriter/script.js
@@ -1,14 +1,13 @@
 const text = document.querySelector('#text');
-const highlightBackground = document.querySelector('.highlight__background');
 const highlightContainer = document.querySelector('.highlight__container');
 const keyDisplay = document.querySelector('#hightlight__key');
 
 const enterKey = (e) => {
     text.innerText += e.key;    
     keyDisplay.innerText = e.key;  
-
-    highlightBackground.classList.add('display');
-    highlightContainer.classList.add('display');
+    
+    highlightContainer.classList.add('transition');
+    keyDisplay.classList.add('display');
 
     switch(e.code){
         case 'Space':
@@ -27,14 +26,9 @@ const enterKey = (e) => {
 };
 
 const exitKey = () => {
-    highlightBackground.classList.remove('display');
-    highlightContainer.classList.remove('display');
+    highlightContainer.classList.remove('transition');
+    keyDisplay.classList.remove('display');
 }
 
 document.addEventListener('keydown', enterKey);
 document.addEventListener('keyup', exitKey);
-
-//event listener for keydown
-    //show modal, key, background
-//event listener for keyup
-    //remove modal, background, key

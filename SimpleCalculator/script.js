c4b377f323c9f89d288315ba7a98531365ab6e2f
ImranRovano <imranhuhn1@gmail.com>
2023-02-23 21:49:17 -0500

    02232023 2149

--- SimpleCalculator/script.js
+++ SimpleCalculator/script.js
@@ -4,7 +4,7 @@
 // Bonus points:
 // Show a red border if the input contains anything else but a number
 
-const displayResult = document.querySelector('#result');
+const display = document.querySelector('#result');
 const firstNum = document.querySelector('#first-number');
 const secondNum = document.querySelector('#second-number');
 const multiply = document.querySelector('#multiplication');
@@ -12,53 +12,11 @@ const subtract = document.querySelector('#subtraction');
 const divide = document.querySelector('#division');
 const sum = document.querySelector('#sumation');
 
+function input(){this.value.trim().length && isNaN(this.value) ? this.classList.add('invalid') : this.classList.remove('invalid');}
 
-
-///////////////////////////////inputs
-// first input
-firstNum.addEventListener('input', () => {
-    if (firstNum.value.trim().length && isNaN(firstNum.value)) {
-        firstNum.classList.add('invalid')
-      } else {
-        firstNum.classList.remove('invalid')
-      }
-})
-
-// second input
-secondNum.addEventListener('input', () => {
-    if (secondNum.value.trim().length && isNaN(secondNum.value)) {
-        secondNum.classList.add('invalid')
-      } else {
-        secondNum.classList.remove('invalid')
-      }
-})
-
-///////////////////////////////calculations
-// multiply button
-multiply.addEventListener('click', () => {
-    const result = parseInt(firstNum.value) * parseInt(secondNum.value);
-    displayResult.innerText = result;
-    return result;
-})
-
-// subtract button
-subtract.addEventListener('click', () => {
-    const result = parseInt(firstNum.value) - parseInt(secondNum.value);
-    displayResult.innerText = result;
-    return result;
-})
-
-// divide button
-divide.addEventListener('click', () => {
-    const result = parseInt(firstNum.value) / parseInt(secondNum.value);
-    displayResult.innerText = result;
-    return result;
-})
-
-// add button
-sum.addEventListener('click', () => {
-    const result = parseInt(firstNum.value) + parseInt(secondNum.value);
-    displayResult.innerText = result;
-    return result;
-})
-
+multiply.addEventListener('click', () => display.innerText = parseInt(firstNum.value) * parseInt(secondNum.value))
+subtract.addEventListener('click', () => display.innerText = parseInt(firstNum.value) - parseInt(secondNum.value))
+divide.addEventListener('click', () => display.innerText = parseInt(firstNum.value) / parseInt(secondNum.value))
+sum.addEventListener('click', () => display.innerText = parseInt(firstNum.value) + parseInt(secondNum.value))
+firstNum.addEventListener('input', input);
+secondNum.addEventListener('input', input);

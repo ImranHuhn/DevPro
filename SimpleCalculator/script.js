125f5c1832e789755a8e7868e617931117e9e648
ImranRovano <imranhuhn1@gmail.com>
2023-02-24 22:52:40 -0500

    02242023 2252

--- SimpleCalculator/script.js
+++ SimpleCalculator/script.js
@@ -1,9 +1,3 @@
-// Requirements:
-// The app should have two inputs
-// The app should have 4 buttons to perform math calculations (addition, subtraction, division, multiplication)
-// Bonus points:
-// Show a red border if the input contains anything else but a number
-
 const display = document.querySelector("#result");
 const firstInput = document.querySelector("#first-number");
 const secondInput = document.querySelector("#second-number");
@@ -12,34 +6,34 @@ const subtract = document.querySelector("#subtraction");
 const divide = document.querySelector("#division");
 const sum = document.querySelector("#sumation");
 
+
 function validator() {
-    if(this.value.length !== 0 && isNaN(this.value)){
-        this.classList.add("invalid")
-    } else {
-        this.classList.remove("invalid");
-    }
+  this.value.length !== 0 && isNaN(this.value)
+    ? this.classList.add("invalid")
+    : this.classList.remove("invalid");
 }
 
 function operation(op) {
-    const a = parseInt(firstInput.value);
-    const b = parseInt(secondInput.value);
-    switch(op) {
-        case "multiply":
-            display.innerText = a * b;
-            break;
-
-        case "subtract":
-            display.innerText = a - b;
-            break;
-            
-        case "divide":
-            display.innerText = a / b;
-            break;
-
-        case "sum":
-            display.innerText = a + b;
-            break;
+  const a = parseInt(firstInput.value);
+  const b = parseInt(secondInput.value);
+  if (!isNaN(a) && !isNaN(b)) {
+    switch (op) {
+      case "multiply":
+        display.innerText = a * b;
+        break;
+      case "subtract":
+        display.innerText = a - b;
+        break;
+      case "divide":
+        display.innerText = a / b;
+        break;
+      case "sum":
+        display.innerText = a + b;
+        break;
     }
+  } else {
+    display.innerText = "ENTER NUMBERS";
+  }
 }
 
 firstInput.addEventListener("input", validator);
@@ -48,3 +42,71 @@ multiply.addEventListener("click", () => {operation("multiply")});
 subtract.addEventListener("click", () => {operation("subtract")});
 divide.addEventListener("click", () => {operation("divide")});
 sum.addEventListener("click", () => {operation("sum")});
+
+
+//////////////////////////////////////////////////////////
+
+
+const special = document.querySelector("#special");
+const start = document.querySelector("#start");
+const sumGame = document.querySelector("#sum-game");
+const subtractGame = document.querySelector("#subtract-game");
+const multiplyGame = document.querySelector("#multiply-game");
+const divisionGame = document.querySelector("#division-game");
+const checkBox = document.querySelectorAll('.game');
+
+
+special.addEventListener("click", () => {
+    document.querySelector('img').classList.add('add-puppet');
+    document.querySelector('.container-calc').classList.add('remove-calc');
+    document.querySelector('.container-game').classList.add('opacity-show');
+});
+
+
+// random operator
+function randomParams() {
+    let randomOp = "";
+    const randomOne = Math.floor(Math.random() * document.querySelector('.number-range').value);
+    const randomTwo = Math.floor(Math.random() * document.querySelector('.number-range').value);
+    const checkArray = [];
+    for(let i = 0; i < checkBox.length; i++){
+        if(checkBox[i].checked === true) {
+            checkArray.push(checkBox[i].value);
+        }
+    }
+    randomOp = checkArray[Math.floor(Math.random() * checkArray.length)];
+    // console.log(checkArray)
+    // console.log(random)
+    console.log(randomOp);
+
+    console.log(randomOne, randomTwo);
+}
+
+
+// random number
+// function randomNumbers() {
+//     const randomOne = Math.floor(Math.random() * document.querySelector('.number-range').value);
+//     const randomTwo = Math.floor(Math.random() * document.querySelector('.number-range').value);
+//     console.log(randomOne, randomTwo);
+//     return randomOne, randomTwo;
+// }
+
+
+
+start.addEventListener("click", () => {
+    // console.log('btn clicked')
+    
+    // if(sumGame.checked){
+    //     console.log(sumGame.value)
+    // } else if(subtractGame.checked){
+    //     console.log(subtractGame.value)
+    // } else if(multiplyGame.checked){
+    //     console.log(multiplyGame.value)
+    // } else if(divisionGame.checked){
+    //     console.log(divisionGame.value)
+    // }
+    randomParams();
+    // randomNumbers();
+    // console.log(document.querySelector('.number-range').value)
+});
+

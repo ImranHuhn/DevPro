94320b4b119ddd4a1e8a245a7236f8e15234add7
ImranRovano <imranhuhn1@gmail.com>
2023-03-01 16:12:28 -0500

    03012023 1612

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -11,9 +11,6 @@ garabage icon
 checkbox
 */
 
-
-// const task = localStorage.getItem('task') || 'Empty Tasks'
-
 const plusIcon = document.querySelector('.plus-icon');
 const garbageIcon = document.querySelector('.garbage-icon');
 const todoList = document.querySelector('.todoapp-body__list');
@@ -26,11 +23,10 @@ function init() {
 }
 
 function render() {
-    let i = 0;
     todoList.innerHTML = '';
     todoInput.value = '';
     taskList.map((task, index) => {
-        i = index;
+        i++;
         todoList.innerHTML += `
         <li class="todoapp-body__items">
             <div>
@@ -38,25 +34,14 @@ function render() {
                 <label for="task">${task}</label>
             </div>
             <div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="garbage-icon w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="garbage-icon garage${index} w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                 </svg>
             </div>
         </li>`
 
-        // todoList.addEventListener('click', (e) => {
-            //     const className = e.target.classList.value;
-            //     // console.log(className);
-            //     if(className.includes(`garbage${index}`)) {
-                //         taskList.splice(index, 1);
-                //         localStorage.setItem('taskList', JSON.stringify(taskList));
-                //         render();
-                //         console.log(taskList);
-                //     }
-                // })
+        deleteItem(`garbage${index}`);
     })
-            // console.log(todoList);
-    deleteItem(i);
 };
 
 function addItem() {
@@ -68,14 +53,13 @@ function addItem() {
         localStorage.setItem('taskList', JSON.stringify(taskList)); 
         render();
     } 
-    // console.log(taskList);
 };
 
 function deleteItem(i) {
     todoList.addEventListener('click', (e) => {
         const className = e.target.classList.value;
         console.log(className);
-        if(className.includes(`garbage-icon`)) {
+        if(className.includes(i)) {
             taskList.splice(i, 1);
             localStorage.setItem('taskList', JSON.stringify(taskList));
             render();

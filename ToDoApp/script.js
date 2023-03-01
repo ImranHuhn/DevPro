851e2ffc88a20faeb3b972e822effe8b3f229191
ImranRovano <imranhuhn1@gmail.com>
2023-03-01 05:53:03 -0500

    03012023 0552

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -26,9 +26,11 @@ function init() {
 }
 
 function render() {
+    let i = 0;
     todoList.innerHTML = '';
     todoInput.value = '';
     taskList.map((task, index) => {
+        i = index;
         todoList.innerHTML += `
         <li class="todoapp-body__items">
             <div>
@@ -36,25 +38,25 @@ function render() {
                 <label for="task">${task}</label>
             </div>
             <div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="garbage-icon garbage${index} w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="garbage-icon w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                 </svg>
             </div>
         </li>`
 
-        deleteItem(index);
         // todoList.addEventListener('click', (e) => {
             //     const className = e.target.classList.value;
-            //     // console.log(className.includes('garbage'));
-            //     if(className.includes('garbage-icon')) {
-                //         // console.log(taskList);
+            //     // console.log(className);
+            //     if(className.includes(`garbage${index}`)) {
                 //         taskList.splice(index, 1);
                 //         localStorage.setItem('taskList', JSON.stringify(taskList));
                 //         render();
+                //         console.log(taskList);
                 //     }
                 // })
     })
-    // console.log(todoList);
+            // console.log(todoList);
+    deleteItem(i);
 };
 
 function addItem() {
@@ -72,12 +74,12 @@ function addItem() {
 function deleteItem(i) {
     todoList.addEventListener('click', (e) => {
         const className = e.target.classList.value;
-        // console.log(className.includes('garbage'));
-        if(className.includes('garbage-icon')) {
-            // console.log(taskList);
+        console.log(className);
+        if(className.includes(`garbage-icon`)) {
             taskList.splice(i, 1);
             localStorage.setItem('taskList', JSON.stringify(taskList));
             render();
+            console.log(taskList);
         }
     })
 };

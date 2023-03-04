50a9ea09e5a0037629e3ab1460725f378797f99a
ImranRovano <imranhuhn1@gmail.com>
2023-03-04 16:28:13 -0500

    04032023 1628

---
+++ ToDoApp/script-v4.js
@@ -0,0 +1,78 @@
+const plusIcon = document.querySelector('.plus-icon');
+const garbageIcon = document.querySelector('.garbage-icon');
+const todoList = document.querySelector('.todoapp-body__list');
+const todoInput = document.querySelector('.todoapp-body__input');
+
+let taskArrayList = JSON.parse(localStorage.getItem('taskArrayList')) || [];
+
+function render() {
+    todoList.innerHTML = '';
+    taskArrayList.forEach((item) => {
+        todoList.innerHTML += `
+        <li class="todoapp-body__items">
+            <div>
+                <input id="taskId${item.currId}" class="task" type="checkbox" ${item.complete === true ? `checked="checked"` : ``}>
+                <label for="task${item.currId}">${item.task}</label>
+            </div>
+            <div>
+                <svg id="garbageId-${item.currId}" class="garbage-icon w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                </svg>
+            </div>
+        </li>`
+    });
+    
+    // delete item
+    document.querySelectorAll('.garbage-icon').forEach(el => {
+        el.addEventListener('click', () => {
+            const uiId = el.id.replace('garbageId-', '');
+            taskArrayList = taskArrayList.filter((item) => {return item.currId != uiId}); // must use !=. if using !==, it gets buggy
+            
+            localStorage.setItem('taskArrayList', JSON.stringify(taskArrayList));
+            render();
+        });
+    });
+
+    // check complete item
+    document.querySelectorAll('.task').forEach(el => {
+        el.addEventListener('click', () => {
+            const uiId = el.id.replace('taskId', '');
+            let targetObject = taskArrayList.filter((item) => {return item.currId == uiId})[0];
+
+        if(targetObject.complete === false) {
+            el.removeAttribute("checked");
+            
+            targetObject.complete = true;
+            localStorage.setItem('taskArrayList', JSON.stringify(taskArrayList));
+        } else {
+            el.setAttribute("checked", "checked");
+
+            targetObject.complete = false;
+            localStorage.setItem('taskArrayList', JSON.stringify(taskArrayList));
+        }
+
+        });
+    });
+
+    // add item
+    plusIcon.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(todoInput.value !== '') {
+            const obj = {
+                currId: new Date(),
+                task: '',
+                complete: false
+            }
+
+            obj.task = todoInput.value;
+            todoInput.value = '';
+            
+            taskArrayList.push(obj);
+            localStorage.setItem('taskArrayList', JSON.stringify(taskArrayList));
+            
+            render();
+        }
+    });
+};
+
+window.addEventListener('load', render);
--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -1,14 +1,14 @@
-const plusIcon = document.querySelector('.plus-icon');
-const garbageIcon = document.querySelector('.garbage-icon');
-const todoList = document.querySelector('.todoapp-body__list');
-const todoInput = document.querySelector('.todoapp-body__input');
+const plusIcon = document.querySelector(".plus-icon");
+const garbageIcon = document.querySelector(".garbage-icon");
+const todoList = document.querySelector(".todoapp-body__list");
+const todoInput = document.querySelector(".todoapp-body__input");
 
-let taskArrayList = JSON.parse(localStorage.getItem('taskArrayList')) || [];
+let taskArrayList = JSON.parse(localStorage.getItem("taskArrayList")) || [];
 
 function render() {
-    todoList.innerHTML = '';
-    taskArrayList.forEach((item) => {
-        todoList.innerHTML += `
+  todoList.innerHTML = "";
+  taskArrayList.forEach((item) => {
+    todoList.innerHTML += `
         <li class="todoapp-body__items">
             <div>
                 <input id="taskId${item.currId}" class="task" type="checkbox" ${item.complete === true ? `checked="checked"` : ``}>
@@ -19,60 +19,54 @@ function render() {
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                 </svg>
             </div>
-        </li>`
+        </li>`;
+  });
+
+  // delete item
+  document.querySelectorAll(".garbage-icon").forEach((el) => {
+    el.addEventListener("click", () => {
+      const uiId = parseInt(el.id.replace("garbageId-", ""));
+      taskArrayList = taskArrayList.filter((item) => item.currId !== uiId); // make sure the typeof is the same in order to use !== which is preferred.
+
+      localStorage.setItem("taskArrayList", JSON.stringify(taskArrayList));
+      render();
     });
-    
-    // delete item
-    document.querySelectorAll('.garbage-icon').forEach(el => {
-        el.addEventListener('click', () => {
-            const uiId = el.id.replace('garbageId-', '');
-            taskArrayList = taskArrayList.filter((item) => {return item.currId != uiId}); // must use !=. if using !==, it gets buggy
-            
-            localStorage.setItem('taskArrayList', JSON.stringify(taskArrayList));
-            render();
-        });
-    });
+  });
 
-    // check complete item
-    document.querySelectorAll('.task').forEach(el => {
-        el.addEventListener('click', () => {
-            const uiId = el.id.replace('taskId', '');
-            let targetObject = taskArrayList.filter((item) => {return item.currId == uiId})[0];
+  // check complete item
+  document.querySelectorAll(".task").forEach((el) => {
+    el.addEventListener("click", () => {
+      const uiId = parseInt(el.id.replace("taskId", ""));
 
-        if(targetObject.complete === false) {
-            el.removeAttribute("checked");
-            
-            targetObject.complete = true;
-            localStorage.setItem('taskArrayList', JSON.stringify(taskArrayList));
-        } else {
-            el.setAttribute("checked", "checked");
-
-            targetObject.complete = false;
-            localStorage.setItem('taskArrayList', JSON.stringify(taskArrayList));
+      taskArrayList = taskArrayList.map((item) => {
+        if (item.currId !== uiId) {
+          item.complete = !item.complete;
         }
-
-        });
+        return item;
+      });
+      
+      localStorage.setItem("taskArrayList", JSON.stringify(taskArrayList));
+      render();
     });
+  });
 
-    // add item
-    plusIcon.addEventListener('click', (e) => {
-        e.preventDefault();
-        if(todoInput.value !== '') {
-            const obj = {
-                currId: new Date(),
-                task: '',
-                complete: false
-            }
+  // add item
+  plusIcon.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (todoInput.value === "") return; // if condition is false, execute all lines after this
+    const obj = {
+      currId: new Date(),
+      task: todoInput.value,
+      complete: false
+    };
 
-            obj.task = todoInput.value;
-            todoInput.value = '';
-            
-            taskArrayList.push(obj);
-            localStorage.setItem('taskArrayList', JSON.stringify(taskArrayList));
-            
-            render();
-        }
-    });
-};
+    todoInput.value = "";
 
-window.addEventListener('load', render);
+    taskArrayList.push(obj);
+    localStorage.setItem("taskArrayList", JSON.stringify(taskArrayList));
+
+    render();
+  });
+}
+
+window.addEventListener("load", render);
